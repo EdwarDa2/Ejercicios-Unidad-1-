7d263e528161930d5337bf27e81571a8ff0fbc17
EdwarDa2 <edwardanielgonzalezhernandez@gmail.com>
2025-11-10 13:41:55 -0600

    Examen unidad 2

--- Composable_APP/Composable_Navegation/app/src/main/java/com/example/EdwarDa/data/Student.kt
+++
@@ -1,7 +0,0 @@
-package com.example.EdwarDa.data
-
-data class Student(
-    val id: Long,
-    val name: String,
-    val description: String,
-)
--- Cuartitos_A_APP/CuartitosA_app/app/src/main/java/com/example/EdwarGonzalez/model/ListaEstudiantes.kt
+++
@@ -1,8 +0,0 @@
-package com.example.EdwarGonzalez.model
-
-data class Student(
-    val id: Int = 1,
-    val name: String = "",
-    val description: String = "",
-    val imgURL: String = "",
-)
